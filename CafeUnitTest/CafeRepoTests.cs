62f078ca1ef7f8e004d9ec6ae8efb5df0a46c140
Jordan Read <jread6477@gmail.com>
2021-07-08 20:52:02 -0400

    added all methods for cafe unit tests

--- CafeUnitTest/CafeRepoTests.cs
+++ CafeUnitTest/CafeRepoTests.cs
@@ -2,6 +2,7 @@
 using CafeRepo;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using System.Collections.Generic;
 
 namespace CafeUnitTest
 {
@@ -18,7 +19,7 @@ namespace CafeUnitTest
         }
 
         [TestMethod]
-        public void Create_()
+        public void AddItemsToMenu_MenuItemIsNull_ReturnFalse()
         {
             //arrange -create any variables we need to test this method
             MenuItem menuItem = null;
@@ -26,8 +27,60 @@ namespace CafeUnitTest
 
             //act - actually calling the method
             bool result = repo.AddItemsToMenu(menuItem);
+
             // assert -making sure the method did what it was supposed to 
             Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void AddItemsToMenu_MenuItemIsNotNull_ReturnTrue()
+        {
+            MenuItem menuItem = new MenuItem("BLT", "bacon lettuce and tomato sandwich", "bacon, lettuce, tomato, bread", 4.99);
+
+            bool result = _repo.AddItemsToMenu(menuItem);
+
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void GetItemByMealNumber_MenuItemExists_ReturnMenuItem()
+        {
+            int mealNumber = 1;
+
+            MenuItem result = _repo.GetItemByMealNumber(mealNumber);
+
+            Assert.AreEqual(result.MealNumber, mealNumber);
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public void GetItemByMealNumber_MenuItemDoesNotExist_ReturnNull()
+        {
+            int mealNumber = 33;
+
+            MenuItem result = _repo.GetItemByMealNumber(mealNumber);
+
+            Assert.IsNull(result);
+        }
+
+        [TestMethod]
+        public void RemoveItemFromMenu_MenuItemIsNull_ReturnFalse()
+        {
+            int mealNumber = 33;
+
+            bool result = _repo.RemoveItemFromMenu(mealNumber);
+
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void RemoveItemFromMenu_MenuItemIsNotNull_ReturnTrue()
+        {
+            int mealNumber = 1;
+
+            bool result = _repo.RemoveItemFromMenu(mealNumber);
+
+            Assert.IsTrue(result);
         }
     }
 }
