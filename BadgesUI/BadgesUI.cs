c38097676060685cc56fb106e953f0137a66ef56
Jordan Read <jread6477@gmail.com>
2021-07-15 19:35:15 -0400

    BadgesUI main menu added.  empty methods thrown.

--- BadgesUI/BadgesUI.cs
+++ BadgesUI/BadgesUI.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BadgesRepo;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,11 +7,64 @@ using System.Threading.Tasks;
 
 namespace BadgesUI
 {
-    class BadgesUI
+    public class BadgesUI
     {
+        private readonly BadgeRepo _badgeRepo = new BadgeRepo();
         public void Run()
         {
             AppMenu();
+        }
+
+        private void AppMenu()
+        {
+            bool keepRunning = true;
+            while (keepRunning)
+            {
+                Console.WriteLine("==============Komodo Badging System===============");
+                Console.WriteLine("Select an option:\n" +
+                    "1.Add a badge\n" +
+                    "2.Edit a badge\n" +
+                    "3.List all badges\n" +
+                    "4.Exit app\n");
+
+                string menuInput = Console.ReadLine();
+                switch (menuInput)
+                {
+                    case "1":
+                        AddBadge();
+                        break;
+                    case "2":
+                        EditBadge();
+                        break;
+                    case "3":
+                        ListAllBadges();
+                        break;
+                    case "4":
+                        Console.WriteLine("Goodbye.");
+                        keepRunning = false;
+                        break;
+                    default:
+                        Console.WriteLine("Please enter a valid number");
+                        break;
+                }
+                Console.WriteLine("\npress any key to continue...");
+                Console.ReadKey();
+                Console.Clear();
+            }
+        }
+        private void AddBadge()
+        {
+
+        }
+
+        private void EditBadge()
+        {
+
+        }
+
+        private void ListAllBadges()
+        {
+
         }
     }
 }
